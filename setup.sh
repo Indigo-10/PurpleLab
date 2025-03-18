726f4590003857cfc6296d3a030b4ce585b69ad2
Indigo-10 <80222231+Indigo-10@users.noreply.github.com>
2025-03-18 17:02:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -126,6 +126,8 @@ Restart=on-failure
 WantedBy=multi-user.target
 EOF
 
+sudo smtpctl update table aliases
+
 sudo tee /etc/ssh/sshd_config <<EOF
 # This is the sshd server system-wide configuration file.  See
 # sshd_config(5) for more information.
